c14fdf314743daa899158a1e699a31b520299bc4
domini04 <sinyubi04@gmail.com>
2025-03-05 17:23:40 +0900

    Add custom filters and actions in admin panel for reviews and rooms

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from . import models
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Search by word"
+    parameter_name = "review"
+    
+    def lookups(self, request, model_admin):
+        return [("good", "Good"), ("great", "Great"), ("awesome", "Awesome")]
+    
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(review__icontains=self.value())
+        return queryset
+
 @admin.register(models.Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ("__str__", "rating", "get_experience_name")
-    list_filter = ("rating",)
+    list_filter = ("rating","user__is_host", "room__category", WordFilter)
     search_fields = ("user__username",)
 
     def get_experience_name(self, obj):
